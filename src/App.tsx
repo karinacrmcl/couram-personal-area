939ad9ae9952d957de5bad122f52dd4165c57671
karinacrmcldev <perfechate@gmail.com>
2022-02-08 16:00:25 +0200

    Added router, some packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { Route, Router, Routes } from "react-router";
 import { Menu } from "./components/menu";
 import HeaderContainer from "./containers/header";
+import Home from "./pages/home";
 import "./shared/styles/global.scss";
 
 function App() {
@@ -8,6 +10,13 @@ function App() {
     <>
       <div className="container">
         <HeaderContainer />
+        <Routes>
+          <Route index element={<Home />} />
+          {/* <Route path="about" element={<About />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="*" element={<NoMatch />} /> */}
+          {/* </Route> */}
+        </Routes>{" "}
       </div>
     </>
   );
