fb408a449066927c808d76d6c76984a83c8593e8
karinacrmcldev <perfechate@gmail.com>
2022-03-07 22:59:37 +0200

    Creating hook for getting one country

--- client/src/hooks/useBasicInfo.ts
+++ client/src/hooks/useBasicInfo.ts
@@ -1,6 +1,29 @@
+import { useEffect, useState } from "react";
 import { useCountries } from "./useCountries";
 
 export function useBasicInfo(name: string) {
-  const country = useCountries();
-  console.log(country);
+  const countries = useCountries();
+  const [item, setItem] = useState({});
+  // console.log(
+  //   countries.data?.filter((item: { name: any }) => item.name === name)
+  // );
+  // useEffect(() => {
+  //   return () => {
+  //     const country = countries?.data?.filter((item: { name: string }) => {
+  //       console.log(item);
+  //       return item.name === name;
+  //     });
+  //     setItem(country);
+  //   };
+  // }, [name]);
+
+  useEffect(() => {
+    return () => {
+      console.log(
+        countries.data.filter((item: { name: any }) => item.name === name)
+      );
+    };
+  }, [countries]);
+
+  return item;
 }
