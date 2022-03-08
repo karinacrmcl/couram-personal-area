5b01ad19c1c335e00dd34d11b8879ff763305854
karinacrmcldev <perfechate@gmail.com>
2022-03-08 23:47:25 +0200

    Creating context for current country data

---
+++ client/provider/current-country.ts
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { CurrentCountryContext } from "../context/current-country";
+
+// export default function CurrentCountryProvider({ children }) {
+//   const [currentCountry, setCurrentCountry] = useState({
+//     data: true,
+//   });
+
+//   return (
+//     <CurrentCountryContext.Provider
+//       value={{ currentCountry, setCurrentCountry }}
+//     >
+//       {children}
+//     </CurrentCountryContext.Provider>
+//   );
+// }
