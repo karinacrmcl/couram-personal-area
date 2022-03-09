859af57b732d5d495680f4eace1c804b96b367c7
karinacrmcldev <perfechate@gmail.com>
2022-03-09 21:51:53 +0200

    Added general types

---
+++ client/src/types/country.ts
@@ -0,0 +1,11 @@
+export type CountryBasicInfo = {
+  name: string;
+  capital: string;
+  languages: Array<string>;
+  totalSize: number;
+  population: number;
+  currencies: Array<string>;
+  subregion: string;
+  climate: string;
+  shortDescription: string;
+};
