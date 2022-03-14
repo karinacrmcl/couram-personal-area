64903ab7a4087f5f31fd5c43c64ca98a284f16a9
karinacrmcldev <perfechate@gmail.com>
2022-03-14 17:11:06 +0200

    Installing and setting up ndoe cron

---
+++ api/core/cron.ts
@@ -0,0 +1,28 @@
+import { cron, CronJob } from "cron";
+import { db } from "../database/createConnection";
+import { Country } from "../model/country.model";
+
+export const job = new CronJob(
+  "0 */6 * * *", //every 6 hours
+  // 0 0 0 * * * every midnight
+  async function () {
+    const countries = await Country.find({});
+
+    const temp = await db.collection("temp");
+    let response = await temp.findOne({});
+    await temp.updateOne(
+      { id: "countryID" },
+      {
+        $set: {
+          currentCountryId:
+            response.currentCountryId <= countries.length - 1
+              ? ++response.currentCountryId
+              : 1,
+        },
+      }
+    );
+  },
+  null,
+  true,
+  "America/Los_Angeles"
+);
