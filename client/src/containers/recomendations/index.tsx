dcf557867b8b0e3e8af42c8ce9ea79bb1a1040a5
karinacrmcldev <perfechate@gmail.com>
2022-03-20 22:53:02 +0200

    Fixes in recomendations component

--- client/src/containers/recomendations/index.tsx
+++ client/src/containers/recomendations/index.tsx
@@ -8,25 +8,43 @@ import SwiperCore, { Navigation } from "swiper";
 import { useMediaQuery } from "react-responsive";
 import { useRecomendations } from "../../hooks/useRecomendations";
 import { CountryBasicInfo } from "../../types/country";
+import { TransitionWrapper } from "../../shared/components/transition";
+import { useExtendedInfoContext } from "../../context/extended-info";
+import "../../shared/styles/swiper.scss";
 SwiperCore.use([Navigation]);
 
 export function RecomendationsContainer() {
   const countries = useRecomendations();
   const isTabletSM = useMediaQuery({ maxWidth: "1100px" });
+  const { isExtendedOpen } = useExtendedInfoContext();
+
+  const transitionStyles = {
+    entering: { opacity: 1, zIndex: 1 },
+    entered: { opacity: 1, zIndex: 1 },
+    exiting: { opacity: 0, height: 0 },
+    exited: { opacity: 0, zIndex: -100, height: 0 },
+  };
 
   return (
     <SectionLayout title="Recomendations">
-      <Swiper navigation={true} slidesPerView={isTabletSM ? 1 : 2}>
-        {countries?.data?.map(
-          (item: CountryBasicInfo, i: React.Key | null | undefined) => {
-            return (
-              <SwiperSlide key={i}>
-                <RecomendedCountry item={item} />
-              </SwiperSlide>
-            );
-          }
-        )}
-      </Swiper>
+      <TransitionWrapper
+        state={isExtendedOpen}
+        transitionStyles={transitionStyles}
+        styles={{ height: "400px" }}
+        duration={200}
+      >
+        <Swiper navigation={true} slidesPerView={isTabletSM ? 1 : 2}>
+          {countries?.data?.map(
+            (item: CountryBasicInfo, i: React.Key | null | undefined) => {
+              return (
+                <SwiperSlide key={i}>
+                  <RecomendedCountry item={item} />
+                </SwiperSlide>
+              );
+            }
+          )}
+        </Swiper>
+      </TransitionWrapper>
     </SectionLayout>
   );
 }
