738914442c22a653405c98f7fd9da2a0671a9b44
karinacrmcldev <perfechate@gmail.com>
2022-03-21 22:48:43 +0200

    Moved styles for swiper library, fixed svg selectors

--- client/src/components/current-country-media/svg-selector.tsx
+++ client/src/components/current-country-media/svg-selector.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function SvgSelector({ id }) {
+export default function SvgSelector({ id }: { id: string }) {
   switch (id) {
     case "question":
       return (
