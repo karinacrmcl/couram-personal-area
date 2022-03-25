aa28ec6174594e12cc4c1361edf181a9f886a56e
karinacrmcldev <perfechate@gmail.com>
2022-03-25 23:33:38 +0200

    Added imports of pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { Route, Router, Routes, useLocation } from "react-router";
 import { Menu } from "./components/menu";
 import HeaderContainer from "./containers/header";
+import AllCountries from "./pages/all-countries";
+import ExploreMore from "./pages/explore";
 import Favourites from "./pages/favourites";
 import Home from "./pages/home";
 import NotFound from "./pages/not-found";
@@ -17,8 +19,8 @@ function App() {
           <Routes>
             <Route index element={<Home />} />
             <Route path="favourites" element={<Favourites />} />
-            {/*  <Route path="dashboard" element={<Dashboard />} />
-           </Route>*/}
+            <Route path="all-countries" element={<AllCountries />} />
+            <Route path="explore-more" element={<ExploreMore />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </ExtendedInfoProvider>
