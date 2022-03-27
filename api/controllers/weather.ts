162a6853b205cebd00c0c5d6b7962e06ed2ae5f3
karinacrmcldev <perfechate@gmail.com>
2022-03-27 23:53:35 +0300

    Fixed controller

--- api/controllers/weather.ts
+++ api/controllers/weather.ts
@@ -3,16 +3,13 @@ import axios from "axios";
 export const getWeather = async (req, res) => {
   const apiUrl = "api.weatherapi.com/v1/";
 
-  try {
-    const cityName = req.query.cityName || "";
-    axios
-      .get(
-        `http://${apiUrl}current.json?key=${process.env.WEATHER_API_KEY}&q=${cityName}&aqi=yes`
-      )
-      .then((response) => {
-        res.send(response.data);
-      });
-  } catch (error) {
-    console.log(error.message);
-  }
+  const cityName = req.query.cityName || "";
+  axios
+    .get(
+      `http://${apiUrl}current.json?key=${process.env.WEATHER_API_KEY}&q=${cityName}&aqi=yes`
+    )
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((e) => console.log(e));
 };
