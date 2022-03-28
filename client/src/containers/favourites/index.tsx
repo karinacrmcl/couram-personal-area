5008ac58c786ce9beb78ef0a22497620aeb7cca4
karinacrmcldev <perfechate@gmail.com>
2022-03-28 22:00:13 +0300

    Favourites component

--- client/src/containers/favourites/index.tsx
+++ client/src/containers/favourites/index.tsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { FavouriteItem } from "../../components/favourite";
 import { useFavourites } from "../../hooks/useFavourites";
-
+import { SectionLayout } from "../../layouts/section";
+import { getStoredIds } from "../../utils/local-storage";
+import s from "./favourites.module.scss";
 type Props = {};
 
 export function FavouritesContainer(props: Props) {
-  const yes = useFavourites([1, 2]);
-  console.log(yes);
-  return <div>FavouritesContainer</div>;
+  const ids = getStoredIds();
+  const favourites = useFavourites(ids);
+
+  return (
+    <SectionLayout title="Your Favourites">
+      <div className={s.favourites}>
+        {favourites.map((item) => {
+          console.log(item);
+          return <FavouriteItem item={item} />;
+        })}
+      </div>
+    </SectionLayout>
+  );
 }
