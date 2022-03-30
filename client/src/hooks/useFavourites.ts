39fad9f87186b6af44cd75f37a7808cf75c2ca19
karinacrmcldev <perfechate@gmail.com>
2022-03-30 22:00:59 +0300

    Fixes in hook

--- client/src/hooks/useFavourites.ts
+++ client/src/hooks/useFavourites.ts
@@ -1,56 +1,14 @@
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
-// import { CountryFullInfo } from "../types/country";
-
-// export function useFavourites(ids: number[]) {
-//   let bar: any = {};
-//   const [favourites, setFavourites] = useState<CountryFullInfo[]>([]);
-
-//   useEffect(() => {
-//     const getFavourite = async (id: number) => {
-//       // console.log("id", id);
-//       axios
-//         .get(`${process.env.REACT_APP_API_URL}/country?id=${id}`, {
-//           headers: {
-//             "Access-Control-Allow-Origin": "*",
-//           },
-//           params: {
-//             id: id,
-//           },
-//         })
-//         .then((res) => {
-//           // console.log("data", res.data);
-//           return res.data;
-//         })
-//         .catch((error) => {
-//           console.log(error);
-//         });
-//     };
-//     return () => {
-//     console.log(getFavourite(1));
-//     };
-
-//     // setFavourites(favourites);
-//     // ids?.map(async (id: number) => {
-//     //   const country: CountryFullInfo = getFavourite(id);
-//     //   setFavourites(favourites.concat(country));
-//     // });
-//   }, [ids]);
-//   // return favourites;
-// }
-//----------------------------------
-
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { CountryFullInfo } from "../types/country";
 
 export function useFavourites(ids: number[]) {
-  let bar: any = {};
-  const [favourites, setFavourites] = useState<CountryFullInfo[]>([]);
+  const [favourites, setFavourites] = useState([]);
+  const [response, setresponse] = useState([]);
 
   useEffect(() => {
     const getFavourites = async (ids: number[]) => {
-      const countries = ids.map((id) => {
+      ids.map((id) => {
         axios
           .get(`${process.env.REACT_APP_API_URL}/country?id=${id}`, {
             headers: {
@@ -61,16 +19,19 @@ export function useFavourites(ids: number[]) {
             },
           })
           .then((res) => {
-            setFavourites(favourites.concat(res.data));
+            setresponse(res.data);
           })
           .catch((error) => {
             console.log(error);
           });
       });
-
-      return countries;
     };
     getFavourites(ids);
   }, []);
+
+  useEffect(() => {
+    setFavourites(favourites.concat(response));
+  }, [response]);
+
   return favourites;
 }
