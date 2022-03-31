d98d10ea9ca1eaf2f846428f86420ca79ae78f87
karinacrmcldev <perfechate@gmail.com>
2022-03-31 22:01:36 +0300

    Created new pages

--- client/src/pages/favourites.tsx
+++ client/src/pages/favourites.tsx
@@ -1,10 +1,28 @@
 import React from "react";
+import { ExplorationContainer } from "../containers/exploration";
 import { FavouritesContainer } from "../containers/favourites";
+import { useExtendedInfoContext } from "../context/extended-info";
+import { TransitionWrapper } from "../shared/components/transition";
 
 export default function Favourites() {
+  const { isExtendedOpen } = useExtendedInfoContext();
+  const transitionStyles = {
+    entering: { opacity: 1, zIndex: 1 },
+    entered: { opacity: 1, zIndex: 1 },
+    exiting: { opacity: 0 },
+    exited: { opacity: 0, zIndex: -100, display: "none" },
+  };
+
   return (
     <>
       <FavouritesContainer />
+      <TransitionWrapper
+        state={!isExtendedOpen}
+        transitionStyles={transitionStyles}
+        duration={200}
+      >
+        <ExplorationContainer />
+      </TransitionWrapper>
     </>
   );
 }
