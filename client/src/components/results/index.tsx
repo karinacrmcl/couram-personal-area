d3379e0cdef82a2e90ddae7034a5e15918cddb70
karinacrmcldev <perfechate@gmail.com>
2022-04-07 14:41:30 +0300

    Created functions for results

--- client/src/components/results/index.tsx
+++ client/src/components/results/index.tsx
@@ -1,5 +1,6 @@
 import classNames from "classnames";
 import React, { ReactNode, useEffect, useState } from "react";
+import { useSearchContext } from "../../context/search";
 import { useCountries } from "../../hooks/useCountries";
 import { Dropdown } from "../../shared/components/dropdown";
 import { FilterRange } from "../../shared/components/range";
@@ -7,63 +8,35 @@ import { filterParams } from "../../shared/constants/searchFilters";
 import { CountryBasicInfo } from "../../types/country";
 import { numberWithCommas } from "../../utils/splitNumber";
 import s from "./results.module.scss";
+import { FilterItem, SelectFilters } from "./select-filters";
 
 type Props = {};
-
-type FilterOptions = {
-  item: {
-    minValue: number;
-    maxValue: number;
-    startValue: number;
-    endValue: number;
-    step: number;
-    unit: string;
-    icon: string;
-  };
-};
-
-function SelectFilters({ item }: FilterOptions) {
-  const [values, setValues] = useState([item.startValue, item.endValue]);
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-
-  function handleSetValues(items: number[]) {
-    setValues(items);
-  }
-
-  function handleOpen() {
-    setIsDropdownOpen(!isDropdownOpen);
-  }
-
-  return (
-    <Dropdown
-      preview={values}
-      isOpen={isDropdownOpen}
-      handleOpen={handleOpen}
-      unit={item.unit}
-      icon={item.icon}
-    >
-      <FilterRange
-        minValue={item.minValue}
-        maxValue={item.maxValue}
-        defValues={values}
-        setNewValues={handleSetValues}
-        step={item.step}
-      />
-    </Dropdown>
-  );
-}
 
 export function Results(props: Props) {
   const countryList = useCountries();
   const previewList = countryList?.data?.slice(0, 10);
+
+  const { searchTerm, setSearchTerm } = useSearchContext();
+
+  function updateSearch(values: number[]) {
+    setSearchTerm({ ...searchTerm, area: values });
+  }
+
+  console.log(searchTerm);
 
   return (
     <div className={s.results}>
       <div className={s.results_top}>
         <h3>Results</h3>
         <div className={s.results_params}>
-          {filterParams.map((item) => {
-            return <SelectFilters item={item} key={item.id} />;
+          {filterParams.map((item: FilterItem) => {
+            return (
+              <SelectFilters
+                item={item}
+                key={item.id}
+                onUpdate={(values) => updateSearch(values)}
+              />
+            );
           })}
         </div>
       </div>
@@ -90,4 +63,7 @@ export function Results(props: Props) {
       </table>
     </div>
   );
+}
+function values(item: FilterItem, values: any) {
+  throw new Error("Function not implemented.");
 }
