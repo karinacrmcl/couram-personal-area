da28449c37d4dc7d8eb6a9ea96824afd2f27ad1f
karinacrmcldev <perfechate@gmail.com>
2022-04-11 23:24:19 +0300

    Fixes in country card component

---
+++ client/src/components/country-card/index.tsx
@@ -0,0 +1,97 @@
+import classNames from "classnames";
+import React, { useState } from "react";
+import { useExtendedInfoContext } from "../../context/extended-info";
+import { CardLayout } from "../../layouts/card";
+import { Button } from "../../shared/components/button";
+import { Favourite } from "../../shared/components/favourite";
+import { TransitionWrapper } from "../../shared/components/transition";
+import GlobalSelector from "../../shared/icons/svg-selector";
+import { CountryBasicInfo } from "../../types/country";
+import { formatString } from "../../utils/format-string";
+import { checkStoredIds, storeId } from "../../utils/local-storage";
+import s from "./country-card.module.scss";
+
+type Props = {
+  item: CountryBasicInfo;
+};
+
+export function CountryCard({ item }: Props) {
+  const transitionStyles = {
+    entering: { opacity: 1, zIndex: 1 },
+    entered: { opacity: 1, zIndex: 1 },
+    exiting: { opacity: 0, height: 0 },
+    exited: {
+      opacity: 0,
+      zIndex: -100,
+      height: 0,
+      display: "none",
+    },
+  };
+
+  const [isExpanded, setisExpanded] = useState(false);
+
+  return (
+    <CardLayout
+      bgUrl={item.coverPhoto.largeUrl}
+      styles={{
+        width: isExpanded ? "850px" : "400px",
+        height: "500px",
+        padding: "0px",
+        justifyContent: "flex-end",
+        boxShadow: "0px 0px 20px rgba(0, 0, 0, 0.19)",
+        borderRadius: "30px",
+      }}
+      type="middle"
+    >
+      <div className={s.countrycard_shadow}></div>
+      <div
+        className={classNames(s.countrycard, {
+          [s.countrycard_expanded]: isExpanded,
+        })}
+      >
+        <Favourite
+          onClick={() => storeId(item?.id)}
+          active={checkStoredIds(item.id)}
+        />
+        <div
+          className={classNames(s.countrycard_content, {
+            [s.countrycard_content_expanded]: isExpanded,
+          })}
+        >
+          <h3>{item.name}</h3>
+          {isExpanded && <br />}
+          <p>Capital: {item.capital}</p>
+
+          <div
+            className={classNames(s.countrycard_content_body, {
+              [s.countrycard_content_body_hidden]: !isExpanded,
+            })}
+          >
+            <p>Language: {item.languages[0]}</p>
+            <p>Currency: {item.currencies[0]}</p>
+            <br />
+            <p>{item.shortDescription}</p>
+          </div>
+
+          <Button
+            size="small"
+            styles={{
+              right: "5%",
+              bottom: "10%",
+              float: "right",
+              position: "absolute",
+              transform: isExpanded ? "rotate(45deg)" : "rotate(0deg)",
+            }}
+            onClick={() => {
+              setisExpanded(!isExpanded);
+            }}
+          >
+            <GlobalSelector id="plus" />
+          </Button>
+        </div>
+      </div>
+    </CardLayout>
+  );
+  {
+  }
+}
