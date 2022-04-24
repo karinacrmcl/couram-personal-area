9ed97fb9e190c41764c7c0c808f233cdf7feb1ec
karinacrmcldev <perfechate@gmail.com>
2022-04-24 22:43:24 +0300

    make it better

--- api/server/index.ts
+++ api/server/index.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import { attachPrivateRoutes } from "./routes";
 import { createConnection } from "../database/createConnection";
 import { job } from "../core/cron";
+import path from "path";
 
 const PORT = process.env.PORT || 3001;
 
@@ -16,6 +17,14 @@ app.listen(PORT, () => {
   attachPrivateRoutes(app);
   createConnection();
   job.start();
+});
+
+const publicPath = path.join(__dirname, "..", "..", "client", "public");
+
+app.use(express.static(publicPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
 });
 
 app.get("/", function (req, res) {
