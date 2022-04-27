3708a52de8736853cdebeed2d13c33f7c214d747
karinacrmcldev <perfechate@gmail.com>
2022-04-27 15:36:56 +0300

    test

---
+++ index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import path from "path";
+import { attachPrivateRoutes } from "./routes";
+import { createConnection } from "./database/createConnection";
+import { job } from "./core/cron";
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+app.use(cors());
+
+const publicPath = path.join(__dirname, "client", "build");
+
+app.use(express.static(publicPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
+
+app.listen(PORT, () => {
+  attachPrivateRoutes(app);
+  createConnection();
+  job.start();
+  console.log(`Server listening on ${PORT}`);
+});
