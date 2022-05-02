bbe8a125afd0c7b9daad6bd4033327053cf9edd3
karinacrmcldev <perfechate@gmail.com>
2022-05-03 01:59:50 +0300

    Fixes in server files, package json

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import express from "express";
 import "dotenv/config";
+import express from "express";
 import cors from "cors";
 import path from "path";
 import { attachPrivateRoutes } from "./routes";
