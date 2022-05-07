daf7d9974d75c426ee1b59000aa22d4b73131f3c
karinacrmcldev <perfechate@gmail.com>
2022-05-07 14:24:08 +0300

    test

--- config/default.ts
+++ config/default.ts
@@ -3,5 +3,6 @@ import "dotenv/config";
 export default {
   port: process.env.PORT,
   host: "couram.herokuapp.com",
-  dbUri: process.env.MONGODB_URI,
+  dbUri:
+    "mongodb+srv://perfechate:177242465820@cluster0.ybrcc.mongodb.net/test?authSource=admin&replicaSet=atlas-sv8uld-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
 };
